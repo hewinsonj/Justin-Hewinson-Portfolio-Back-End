053354e35f72d0c481f57279bc6d8f0d448e3026
hewinsonj <hewinsonj@gmail.com>
2022-11-12 23:55:36 -0500

    still trying to get side bar to work. Achieved full CRUD on projects and comments.

---
+++ app/models/message.js
@@ -0,0 +1,31 @@
+
+const mongoose = require('mongoose')
+
+
+const messageSchema = new mongoose.Schema(
+    {
+        recipient: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true 
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        },
+        content: {
+            type: String,
+            required: true,
+            default: 'would love to connect'
+        }
+        
+    },
+    {
+        timestamps: true,
+        toObject: { virtuals: true },
+        toJSON: {virtuals: true }
+    }
+)
+
+module.exports = mongoose.model('Message', messageSchema)
