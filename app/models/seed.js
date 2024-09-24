248b141ecf094a490870818339e0463f67921049
hewinsonj <hewinsonj@gmail.com>
2024-09-24 01:25:24 -0400

    has been redeployed. seed script is commented out, works, ready for clean up.

--- app/models/seed.js
+++ app/models/seed.js
@@ -1,54 +1,54 @@
 // added this seed file to test all activity routes
 
-const mongoose = require('mongoose')
-const Project = require('./project')
-const db = require('../../config/db')
+// const mongoose = require('mongoose')
+// const Project = require('./project')
+// const db = require('../../config/db')
 
-const startProjects = [
-    {    
-        projTitle: 'MyTiki App',
-        img1: 'https://i.imgur.com/GWkVhJO.jpg',
-        img2: 'https://i.imgur.com/GWkVhJO.jpg',
-        img3: 'https://i.imgur.com/GWkVhJO.jpg',
-        description: 'MyTiki is an app that provides 10 classictiki cocktail recipes that are available to be viewed by any logged in user. The user is also able to customize any of the provided recipies and add them to their own original cocktails folder, or create their own from scratch! The user also has the ability to filter the shown cocktails by keywords, and add any cocktails they wish to their favorites folder.',
-        finishDate: '09/12/23',
-        startDate: '09/12/23',
-        link1: 'https://www.linkedin.com/in/justin-hewinson/',
-        link2: 'https://www.linkedin.com/in/justin-hewinson/',
-        link3: 'https://www.linkedin.com/in/justin-hewinson/',
-        role: 'Full Stack Dev',
-        client: 'HTML5, CSS, Bootstrap, JavaScript, LiquidJs, Express, MongoDB, Mongoose, Morgan, Bcryptjs',
-        progress: 10,
-        priority: 'a',
-        private: false,
-    },
-]
+// const startProjects = [
+//     {    
+//         projTitle: 'MyTiki App',
+//         img1: 'https://i.imgur.com/GWkVhJO.jpg',
+//         img2: 'https://i.imgur.com/GWkVhJO.jpg',
+//         img3: 'https://i.imgur.com/GWkVhJO.jpg',
+//         description: 'MyTiki is an app that provides 10 classictiki cocktail recipes that are available to be viewed by any logged in user. The user is also able to customize any of the provided recipies and add them to their own original cocktails folder, or create their own from scratch! The user also has the ability to filter the shown cocktails by keywords, and add any cocktails they wish to their favorites folder.',
+//         finishDate: '09/12/23',
+//         startDate: '09/12/23',
+//         link1: 'https://www.linkedin.com/in/justin-hewinson/',
+//         link2: 'https://www.linkedin.com/in/justin-hewinson/',
+//         link3: 'https://www.linkedin.com/in/justin-hewinson/',
+//         role: 'Full Stack Dev',
+//         client: 'HTML5, CSS, Bootstrap, JavaScript, LiquidJs, Express, MongoDB, Mongoose, Morgan, Bcryptjs',
+//         progress: 10,
+//         priority: 'a',
+//         private: false,
+//     },
+// ]
 
-// connect to database
-mongoose.connect(db, {
-    useNewUrlParser: true
-})
-    .then(() => {
-        Project.deleteMany({ owner: null })
-            .then(deletedProjects => {
-                console.log('deletedProjects', deletedProjects)
+// // connect to database
+// mongoose.connect(db, {
+//     useNewUrlParser: true
+// })
+//     .then(() => {
+//         Project.deleteMany({ owner: null })
+//             .then(deletedProjects => {
+//                 console.log('deletedProjects', deletedProjects)
 
-                Project.create(startProjects)
-                    .then(newProjects => {
-                        console.log('the new Projects', newProjects)
-                        mongoose.connection.close()
-                    })
-                    .catch(error => {
-                        console.log(error)
-                        mongoose.connection.close()
-                    })
-            })
-            .catch(error => {
-                console.log(error)
-                mongoose.connection.close()
-            })
-    })
-    .catch(error => {
-        console.log(error)
-        mongoose.connection.close()
-    })
+//                 Project.create(startProjects)
+//                     .then(newProjects => {
+//                         console.log('the new Projects', newProjects)
+//                         mongoose.connection.close()
+//                     })
+//                     .catch(error => {
+//                         console.log(error)
+//                         mongoose.connection.close()
+//                     })
+//             })
+//             .catch(error => {
+//                 console.log(error)
+//                 mongoose.connection.close()
+//             })
+//     })
+//     .catch(error => {
+//         console.log(error)
+//         mongoose.connection.close()
+//     })
--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 'use strict'
-require("dotenv").config()
+// require("dotenv").config()
 // creating a base name for the mongodb
 // REPLACE THE STRING WITH YOUR OWN DATABASE NAME
 
@@ -15,16 +15,38 @@ require("dotenv").config()
 // };
 
 
-const mongooseBaseName = "hewinsonportfolioapi";
+// const mongooseBaseName = "hewinsonportfolioapi";
 
+
+// // create the mongodb uri for development and test
+// const database = {
+// // 	production: `https://hewinsonportfolioapi.fly.dev`,
+// // 	development: 'http://localhost:8000',
+// 	development: `mongodb+srv://frenchfry:frenchfry@cluster0.4tmzof4.mongodb.net/hewinsonportfolioapi?retryWrites=true&w=majority`,
+//     test: `mongodb+srv://frenchfry:frenchfry@cluster0.4tmzof4.mongodb.net/${mongooseBaseName}?retryWrites=true&w=majority`,
+// };
+
+// // Identify if development environment is test or development
+// // select DB based on whether a test file was executed before `server.js`
+// const localDb = process.env.TESTENV ? database.test : database.development
+
+// // Environment variable MONGODB_URI will be available in
+// // heroku production evironment otherwise use test or development db
+// const currentDb = process.env.MONGODB_URI || localDb
+
+// module.exports = currentDb
+
+
+
+// creating a base name for the mongodb
+// REPLACE THE STRING WITH YOUR OWN DATABASE NAME
+const mongooseBaseName = 'hewinsonportfolioapi'
 
 // create the mongodb uri for development and test
 const database = {
-// 	production: `https://hewinsonportfolioapi.fly.dev`,
-// 	development: 'http://localhost:8000',
-	development: `mongodb+srv://frenchfry:frenchfry@cluster0.4tmzof4.mongodb.net/hewinsonportfolioapi?retryWrites=true&w=majority`,
-    test: `mongodb+srv://frenchfry:frenchfry@cluster0.4tmzof4.mongodb.net/${mongooseBaseName}?retryWrites=true&w=majority`,
-};
+	development: `mongodb://localhost/${mongooseBaseName}-development`,
+	test: `mongodb://localhost/${mongooseBaseName}-test`,
+}
 
 // Identify if development environment is test or development
 // select DB based on whether a test file was executed before `server.js`
